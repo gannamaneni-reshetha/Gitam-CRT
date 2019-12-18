3cef57c1f980d1379448fc2521eb5ff5fade84eb
gannamaneni-reshetha <58929970+gannamaneni-reshetha@users.noreply.github.com>
2019-12-18 12:24:48 +0530

    Add files via upload

---
+++ ve.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+	scanf("%c",&ch);
+	switch(ch)
+{
+	case'a':
+		case'e':
+		    case'i':
+			   case'o':
+				 case'u':
+					case'A':
+						case'E':
+							case'I':
+								case'O':
+									case'U':
+										print("it is vowel");
+										break;
+										default:
+											printf("it is constant");
+											break;
+										}
+										return 0;
+									}
